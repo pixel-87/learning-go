84eef368dd0cfad23aa7fb8328fb464f4d0608a9
pixel <edwardoliverthomas@gmail.com>
2025-12-22 23:04:13 +0000

    chal:6 - inprogress

---
+++ challenges/07-loops/06-connections/connections.go
@@ -0,0 +1,5 @@
+package main
+
+// Implement the challenge here.
+// This file was generated from the challenge title.
+func main() {}
---
+++ challenges/07-loops/06-connections/connections_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestPlaceholder(t *testing.T) {
+    t.Log("replace this with real tests")
+}
