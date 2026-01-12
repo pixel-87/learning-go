4c1cf0d2a833cb422d80da71bfb4240af66d8722
pixel <edwardoliverthomas@gmail.com>
2026-01-12 16:04:53 +0000

    chal:11

---
+++ challenges/10-pointers/11-update-balance/update_balance.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"errors"
+)
+
+type customer struct {
+	id      int
+	balance float64
+}
+
+type transactionType string
+
+const (
+	transactionDeposit    transactionType = "deposit"
+	transactionWithdrawal transactionType = "withdrawal"
+)
+
+type transaction struct {
+	customerID      int
+	amount          float64
+	transactionType transactionType
+}
+
+// Don't touch above this line
+
+func updateBalance (c *customer, t transaction) error {
+	switch  t.transactionType {
+	case transactionDeposit:
+		c.balance += t.amount
+	case transactionWithdrawal:
+		if c.balance - t.amount < 0 {
+			return errors.New("insufficient funds")
+		}
+		c.balance -= t.amount
+	default:
+		return errors.New("unknown transaction type")
+	}
+	return nil
+}
